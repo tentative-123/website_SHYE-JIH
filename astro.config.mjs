00e05a7525aa1e711cc482711b573cde6a4b9d87
tentative-123 <tentative-123@users.noreply.github.com>
2026-04-06 15:19:35 +0800

    fix: configure worker static assets deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-﻿import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: "static",
-  adapter: cloudflare({ platformProxy: { enabled: true } }),
   integrations: [react()]
 });
